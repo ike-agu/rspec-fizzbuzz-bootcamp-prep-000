cc7d62d1ce10b63e7dc3ac3e110594dc676aaf14
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-25 20:26:26 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,9 @@ require_relative './spec_helper.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
+     if fizzbuzz /3 
+       fizz 
+     end 
 
     expect(fizz_3).to eq("Fizz")
   end
